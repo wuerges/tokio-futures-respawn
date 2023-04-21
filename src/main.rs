958ca2f34ebe31198b861486b486a6b1c8ddc4a3
Emilio Wuerges <wuerges@gmail.com>
2023-04-21 12:14:44 -0300

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+use std::fmt::Debug;
 use std::future::Future;
 use std::io;
 use std::panic;
-use tokio::task;
 use tokio::time::sleep;
 use tracing::*;
 use tracing_subscriber::*;
@@ -10,46 +10,48 @@ fn make_future() -> impl Future<Output = Result<i32, io::Error>> {
     async { panic!("boom") }
 }
 
-async fn spawn_future_retry_panics<T, F, Fut>(f: F)
+async fn sleep_1_second() {
+    sleep(std::time::Duration::from_secs(1)).await;
+}
+
+async fn make_future_respawnable<T, E, F, Fut>(f: F)
 where
-    T: Send + 'static,
+    E: std::error::Error + Send + 'static,
+    T: Send + 'static + Debug,
     F: Fn() -> Fut,
-    Fut: Future<Output = T> + Send + 'static,
+    Fut: Future<Output = Result<T, E>> + Send + 'static,
 {
     loop {
         let handle = tokio::spawn(f());
         match handle.await {
-            Ok(r) => {
-                sleep(std::time::Duration::from_secs(1)).await;
-            }
+            Ok(r) => match r {
+                Ok(t) => {
+                    info!(?t, "respawnable future finished");
+                }
+                Err(e) => {
+                    info!(?e, "respawnable future finished with error");
+                }
+            },
             Err(e) => {
                 if e.is_panic() {
-                    sleep(std::time::Duration::from_secs(1)).await;
+                    error!(?e, "respawnable future aborted with panic");
+                } else {
+                    error!(?e, "respawnable future aborted with error");
                 }
             }
         }
+        sleep_1_second().await;
     }
 }
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    // let join_handle: task::JoinHandle<Result<i32, io::Error>> = tokio::spawn(async {
-    //     panic!("boom");
-    // });
-
     let subscriber = FmtSubscriber::builder()
         .with_max_level(Level::TRACE)
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
-    let join_handle = tokio::spawn(spawn_future_retry_panics(make_future));
+    let join_handle = tokio::spawn(make_future_respawnable(make_future));
 
-    let result = join_handle.await;
-    match result {
-        Ok(x) => Ok(x),
-        Err(err) => {
-            sleep(std::time::Duration::from_secs(1)).await;
-            Err(err.into())
-        }
-    }
+    Ok(join_handle.await?)
 }

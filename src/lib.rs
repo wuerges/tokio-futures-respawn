87eed432e2c5688e9724ff921d4c85b95780a227
Emilio Wuerges <wuerges@gmail.com>
2023-04-21 17:13:33 -0300

    tokio retry

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,20 @@ use std::pin::Pin;
 use std::time::Duration;
 use tokio::task::JoinError;
 use tokio::time::sleep;
+
+#[cfg(feature = "tracing")]
 use tracing::{error, info};
 
 pub type RetryResult<T> = Result<T, JoinError>;
 
 pub enum RetryPolicy<T> {
-    Quit(Result<T, JoinError>),
+    Quit(T),
     Repeat,
     WaitRepeat(Duration),
 }
 
-pub trait ErrorHandler<T> {
-    fn handle(&mut self, result: Result<T, JoinError>) -> RetryPolicy<T>;
+pub trait ErrorHandler<T, Q> {
+    fn handle(&mut self, result: Result<T, JoinError>) -> RetryPolicy<Q>;
 }
 
 pub trait FutureFactory<T>
@@ -25,10 +27,7 @@ where
     fn build_future(&mut self) -> Pin<Box<dyn Future<Output = T> + Send>>;
 }
 
-pub async fn make_future_respawnable<T, F>(
-    mut handler: impl ErrorHandler<T>,
-    mut f: F,
-) -> RetryResult<T>
+pub async fn make_future_respawnable<T, Q, F>(mut handler: impl ErrorHandler<T, Q>, mut f: F) -> Q
 where
     T: Send + 'static + Debug,
     F: FutureFactory<T>,
@@ -50,8 +49,8 @@ pub struct AlwaysRespawn {
     duration: std::time::Duration,
 }
 
-impl<T> ErrorHandler<T> for AlwaysRespawn {
-    fn handle(&mut self, _result: Result<T, JoinError>) -> RetryPolicy<T> {
+impl<T> ErrorHandler<T, ()> for AlwaysRespawn {
+    fn handle(&mut self, _result: Result<T, JoinError>) -> RetryPolicy<()> {
         RetryPolicy::WaitRepeat(self.duration)
     }
 }
@@ -62,8 +61,8 @@ pub struct AlwaysRespawnAndTrace {
 }
 
 #[cfg(feature = "tracing")]
-impl<T: Debug, E: std::error::Error> ErrorHandler<Result<T, E>> for AlwaysRespawnAndTrace {
-    fn handle(&mut self, result: Result<Result<T, E>, JoinError>) -> RetryPolicy<Result<T, E>> {
+impl<T: Debug, E: std::error::Error> ErrorHandler<Result<T, E>, ()> for AlwaysRespawnAndTrace {
+    fn handle(&mut self, result: Result<Result<T, E>, JoinError>) -> RetryPolicy<()> {
         match result {
             Ok(finished) => match finished {
                 Ok(ok) => {
@@ -90,8 +89,8 @@ pub struct RetriesTimes {
     count: usize,
 }
 
-impl<T> ErrorHandler<T> for RetriesTimes {
-    fn handle(&mut self, result: Result<T, JoinError>) -> RetryPolicy<T> {
+impl<T> ErrorHandler<T, Result<T, JoinError>> for RetriesTimes {
+    fn handle(&mut self, result: Result<T, JoinError>) -> RetryPolicy<Result<T, JoinError>> {
         if self.count == 0 {
             RetryPolicy::Quit(result)
         } else {
@@ -104,12 +103,11 @@ impl<T> ErrorHandler<T> for RetriesTimes {
 #[cfg(test)]
 mod tests {
     use tracing::info;
-    use tracing_subscriber::FmtSubscriber;
 
     use super::*;
-    use std::panic;
     use std::sync::atomic::{AtomicU32, Ordering};
     use std::sync::Arc;
+    use std::{io, panic};
 
     #[derive(Clone, Debug)]
     struct Dummy;
@@ -130,28 +128,72 @@ mod tests {
 
     #[tokio::test]
     async fn tests_retries_3_times() {
-        let subscriber = FmtSubscriber::builder()
-            .with_max_level(tracing::Level::TRACE)
-            .finish();
-        tracing::subscriber::set_global_default(subscriber)
-            .expect("setting default subscriber failed");
-
-        let dummy = Dummy;
         let count = Arc::new(AtomicU32::new(0));
         let takes_dummy = TakesDummy {
-            dummy,
+            dummy: Dummy,
             count: count.clone(),
         };
 
         let handler = RetriesTimes {
-            duration: std::time::Duration::from_millis(10),
+            duration: std::time::Duration::from_millis(1),
             count: 3,
         };
 
-        let join_handle = tokio::spawn(make_future_respawnable::<(), _>(handler, takes_dummy));
+        let join_handle = tokio::spawn(make_future_respawnable::<(), _, _>(handler, takes_dummy));
 
         let err = join_handle.await.unwrap().unwrap_err();
         assert!(err.is_panic());
         assert_eq!(count.load(Ordering::SeqCst), 4);
+    }
+
+    #[tokio::test]
+    async fn check_always_respawn_compiles() {
+        let count = Arc::new(AtomicU32::new(0));
+        let takes_dummy = TakesDummy {
+            dummy: Dummy,
+            count: count.clone(),
+        };
+
+        let handler = AlwaysRespawn {
+            duration: std::time::Duration::from_millis(1),
+        };
+
+        let join_handle = tokio::spawn(make_future_respawnable::<(), _, _>(handler, takes_dummy));
+
+        sleep(Duration::from_millis(10)).await;
+
+        join_handle.abort();
+
+        let err = join_handle.await.unwrap_err();
+        assert!(!err.is_panic(), "{:?}", err);
+        assert!(err.is_cancelled(), "{:?}", err);
+    }
+
+    struct TaskReturnsResult;
+
+    impl FutureFactory<Result<(), io::Error>> for TaskReturnsResult {
+        fn build_future(&mut self) -> Pin<Box<dyn Future<Output = Result<(), io::Error>> + Send>> {
+            Box::pin(async { panic!("boom") })
+        }
+    }
+
+    #[cfg(feature = "tracing")]
+    #[tokio::test]
+    async fn check_always_respawn_and_trace_compiles() {
+        let factory = TaskReturnsResult;
+
+        let handler = AlwaysRespawnAndTrace {
+            duration: std::time::Duration::from_millis(1),
+        };
+
+        let join_handle = tokio::spawn(make_future_respawnable::<_, _, _>(handler, factory));
+
+        sleep(Duration::from_millis(10)).await;
+
+        join_handle.abort();
+
+        let err = join_handle.await.unwrap_err();
+        assert!(!err.is_panic(), "{:?}", err);
+        assert!(err.is_cancelled(), "{:?}", err);
     }
 }
